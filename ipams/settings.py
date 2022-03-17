57e3b2857837fbc38874c457697fb7183e8a2fd9
leam123 <garcia.leamor1@gmail.com>
2022-03-18 05:19:12 +0800

    Chang database to sqlite

--- ipams/settings.py
+++ ipams/settings.py
@@ -73,23 +73,23 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ipamsdjango',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'my.ini'),
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'mydatabase',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ipamsdjango',
+#         'OPTIONS': {
+#             'read_default_file': os.path.join(BASE_DIR, 'my.ini'),
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
 
 
 # Password validation
