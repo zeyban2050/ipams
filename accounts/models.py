de39e47922bda9d187e98c766e564fe5a847a7f7
yangx42 <yangcotristanjosh@gmail.com>
2022-10-17 16:29:37 +0800

    Code Optimization for Role Request and Signup

--- accounts/models.py
+++ accounts/models.py
@@ -70,7 +70,7 @@ class Department(models.Model):
 		return self.name
 
 class Course(models.Model): #Program
-	name = models.CharField(max_length=100, unique=True)
+	name = models.CharField(max_length=255, unique=True)
 	department = models.ForeignKey(Department, on_delete=models.CASCADE)
 	date_created = models.DateTimeField(auto_now_add=True)
 
